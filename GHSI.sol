d1ba9481b8b0e7459a98ae140a61e246cfca258e
tlcghost <tlcghost@proton.me>
2023-06-10 15:53:46 -0700

    Fix burn

--- GHSI.sol
+++ GHSI.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.18;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GHSI is ERC20, Ownable {
+contract GHSI is ERC20, ERC20Burnable, Ownable {
     uint256 constant maxSupply = 100000000 * (10 ** 18); // 100,000,000 GHSI
 
-    constructor() ERC20('GHSI', 'GHSI') {
+    constructor() ERC20("GHSI", "GHSI") {
         _mint(msg.sender, maxSupply);
-    }
-
-    function burn(uint256 amount) public onlyOwner {
-        _burn(address(this), amount);
     }
 
     function revokeOwnership() public onlyOwner {
